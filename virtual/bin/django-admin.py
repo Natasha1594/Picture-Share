ccb25694f63c0a74538a5eb216918497867e9343
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-05-13 21:55:09 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/armidad/Desktop/natasha/Picture-Globe-master/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
